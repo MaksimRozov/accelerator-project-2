da8e824ae45d5e7dae3654f91ce854c42afc8d9b
Максим <sterh84@gmail.com>
2024-08-29 16:10:24 +0300

    сделан стайлгайд

--- source/js/modules/sliders/slider-main/index.js
+++ source/js/modules/sliders/slider-main/index.js
@@ -5,11 +5,13 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 
 const mainCarouselElement = document.querySelector('.main-carousel');
+const swiperLinks = mainCarouselElement.querySelectorAll('a[href]');
+
 
 const sliderMain = () => {
   if (mainCarouselElement) {
     // eslint-disable-next-line no-unused-vars
-    const swiper = new Swiper(mainCarouselElement, {
+    const swiperMain = new Swiper(mainCarouselElement, {
       modules: [Pagination],
       grabCursor: false,
       watchOverflow: true,
@@ -20,12 +22,42 @@ const sliderMain = () => {
         bulletClass: 'main-carousel__dots',
         bulletActiveClass: 'main-carousel__dots--active',
         clickable: true,
-        bulletElement: 'button type="button"'
+        bulletElement: 'button type="button" tabindex="0"'
       },
 
     });
+
+
+    const setTabIndex = () => {
+      swiperMain.slides[swiperMain.activeIndex].querySelector('a').setAttribute('tabindex', '0');
+    };
+
+    for (const link of swiperLinks) {
+      link.setAttribute('tabindex', '-1');
+      setTabIndex();
+    }
+
+    swiperMain.on('activeIndexChange', () => {
+      for (const link of swiperLinks) {
+        link.setAttribute('tabindex', '-1');
+      }
+      setTabIndex();
+    });
   }
 };
+
+
+// for (const link of swiperLinks) {
+//   link.setAttribute('tabindex', '-1');
+//   setTabIndex();
+// }
+
+// swiper.on('activeIndexChange', () => {
+//   for (const link of swiperLinks) {
+//     link.setAttribute('tabindex', '-1');
+//   }
+//   setTabIndex();
+// });
 
 
 export { sliderMain };
