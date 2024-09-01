07b9dec007670f8f6d33a46cec0231b5c0df8608
Максим <sterh84@gmail.com>
2024-09-01 18:00:52 +0300

    переделал блок преимущества

--- source/js/modules/menu-mobile/index.js
+++ source/js/modules/menu-mobile/index.js
@@ -1,28 +1,27 @@
-const bodyElement = document.querySelector('.page__body');
-const headerElement = document.querySelector('.main-header');
-const menuElement = headerElement.querySelector('.menu');
-const buttonBurger = headerElement?.querySelector('.main-header__burger');
-const menuLinks = headerElement?.querySelectorAll('.menu__link');
+import { bodyElement, menuElement, buttonBurger, menuLinks } from './variables';
 
+const toggleClass = () => {
+  const isOpen = menuElement.classList.toggle('main-header__navigation--open');
+  bodyElement.classList.toggle('page__body--lock', isOpen);
+  buttonBurger.classList.toggle('main-header__burger--open', isOpen);
+};
+
+const removeClass = () => {
+  menuElement.classList.remove('main-header__navigation--open');
+  bodyElement.classList.remove('page__body--lock');
+  buttonBurger.classList.remove('main-header__burger--open');
+};
 
 const initNav = () => {
-  if (!headerElement || !buttonBurger || !menuLinks) {
+  if (!buttonBurger) {
     return;
   }
 
-  const toggleNav = () => {
-    const isOpen = menuElement.classList.toggle('main-header__navigation--open');
-    bodyElement.classList.toggle('page__body--lock', isOpen);
-    buttonBurger.classList.toggle('main-header__burger--open', isOpen);
-  };
-
-  buttonBurger.addEventListener('click', toggleNav);
+  buttonBurger.addEventListener('click', toggleClass);
 
   menuLinks.forEach((link) => {
     link.addEventListener('click', () => {
-      menuElement.classList.remove('main-header__navigation--open');
-      bodyElement.classList.remove('page__body--lock');
-      buttonBurger.classList.remove('main-header__burger--open');
+      removeClass();
     });
   });
 };
---
+++ source/js/modules/menu-mobile/variables.js
@@ -0,0 +1,5 @@
+export const bodyElement = document.querySelector('.page__body');
+export const headerElement = document.querySelector('.main-header');
+export const menuElement = headerElement.querySelector('.menu');
+export const buttonBurger = headerElement?.querySelector('.main-header__burger');
+export const menuLinks = headerElement?.querySelectorAll('.menu__link');
